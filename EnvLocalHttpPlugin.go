9e21c28ef9836d2a7923c0e1c0266bbe9f6b2f5a
judicieux <r4nd0mmachine@gmail.com>
2024-11-04 22:35:58 +0100

    Update EnvLocalHttpPlugin.go

--- EnvLocalHttpPlugin.go
+++ EnvLocalHttpPlugin.go
@@ -17,7 +17,7 @@ func (EnvLocalHttpPlugin) GetVersion() (int, int, int) {
 func (EnvLocalHttpPlugin) GetRequests() []l9format.WebPluginRequest {
         return []l9format.WebPluginRequest{{
                 Method:  "GET",
-                Path:    "/.env.example",
+                Path:    "/.env.local",
                 Headers: map[string]string{},
                 Body:    []byte(""),
         }}
